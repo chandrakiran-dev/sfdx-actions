e2be315af3736c25e12c37f4dd09b456d40b98ba
Chandra Kiran <chandrakiran.vish.dev@gmail.com>
2021-06-28 17:09:32 +0530

    Added sdfds

--- get-user-info/src/action.js
+++ get-user-info/src/action.js
@@ -1,12 +1,17 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { Octokit } from '@octokit/rest';
 
 async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const octokit = new Octokit({
+        auth: GITHUB_TOKEN,
+    });
+      
     console.log('loaded octokit');
 
-    const { data } = await octokit.request("GET /user");
+    const { data } = await octokit.request("/user");
     console.log('User data', data);
     core.setOutput(OUTPUT_USER, data);
 }
